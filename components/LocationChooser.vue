ede4a43f731e2fef70034ca6a48f4c2c1857a3e6
daniel <daniel.janz@dbi-analytics.de>
2019-04-12 00:26:49 +0200

    * Added the following yarn dependencies: vue-select, nuxt-fontawesome, leaflet-routing-machine and their peer dependencies * Created a 'LocationChooser' component to choose the route's start end destination in 'pages/map.vue' (#5) * Implemented #6 in 'components/LocationChooser.vue' (used when "useCurrentPosition" = true) * Implemented routing from start to destination (#8) in 'pages/map.vue'

---
+++ components/LocationChooser.vue
@@ -0,0 +1,122 @@
+<template>
+  <div>
+    <article class="message">
+      <div class="message-header" v-on:click="reset()">
+        <p>{{!location ? title : shortTitle + ": " + location.label}}</p>
+        <span v-show="!isExpanded && location" v-on:click="accept()" class="icon is-medium">
+          <font-awesome-icon style="color:#0ad338;" icon="check"></font-awesome-icon>
+        </span>
+        <span v-show="!isExpanded && !location" v-on:click="accept()" class="icon is-medium">
+          <font-awesome-icon style="color:#cd480f;" icon="question"></font-awesome-icon>
+        </span>
+      </div>
+      <div v-show="isExpanded" class="message-body">
+        <div class="level">
+          <no-ssr>
+            <v-select taggable
+                      v-model="location"
+                      v-on:change="chosenLocationChanged"
+                      :placeholder="'Choose or type a location'"
+                      :options="recentLocations"
+                      :searchable="true">
+              <template slot="option" slot-scope="option">
+                {{ option.label}}
+              </template>
+            </v-select>
+          </no-ssr>
+          <span v-show="location" v-on:click="accept()" class="icon is-medium">
+            <font-awesome-icon style="color:#0ad338;" icon="check"></font-awesome-icon>
+          </span>
+        </div>
+      </div>
+    </article>
+  </div>
+</template>
+
+<script>
+  //import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from 'nuxt-fortawesome'
+
+  export default {
+    mounted() {
+      this.$nextTick(() => {
+        if (!this.useCurrentPosition) {
+          return;
+        }
+
+        let self = this;
+
+        // Get the user's current location.
+        // If possible, set it as start coordinates, else just ignore the error and let the user choose.
+        navigator.geolocation.getCurrentPosition(function (pos) {
+          self.location = {
+            label: 'Your current location',
+            lat: pos.coords.latitude,
+            lng: pos.coords.longitude
+          };
+        }, function () {
+          // TODO: This is for testing only! Remove afterwards
+          self.location = self.recentLocations[0];
+        });
+
+      })
+    },
+    props: [
+      "title",
+      "shortTitle",
+      "useCurrentPosition",
+      "startExpanded"
+    ],
+    name: "LocationChooser",
+    mounted() {
+      this.isExpanded = this.startExpanded;
+    },
+    methods: {
+      chosenLocationChanged(loc) {
+        this.chosenLocation = loc;
+        this.$emit('locationChanged', this.location);
+      },
+      accept() {
+        if (!this.location) {
+          return;
+        }
+        this.$emit('accept', this.location);
+        this.isExpanded = false;
+      },
+      reset() {
+        this.$emit('reset');
+        this.isExpanded = true;
+      }
+    },
+    data() {
+      let data = {
+        recentLocations: [
+          {
+            label: "Adlzreiterstraße",
+            lat: 48.125841,
+            lng: 11.555695
+          },
+          {
+            label: "Aventinstraße",
+            lat: 48.133041,
+            lng: 11.579294
+          },
+          {
+            label: "Balanstraße",
+            lat: 48.117359,
+            lng: 11.601246
+          }
+        ],
+        location: null,
+        isExpanded: false
+      };
+
+      return data;
+    }
+  }
+</script>
+
+<style scoped>
+  .dropdown {
+    width: 100%;
+  }
+</style>
--- pages/map.vue
+++ pages/map.vue
@@ -1,53 +1,94 @@
 <template>
-  <div id="map-wrap" ref="leafletMap" style="height: 100vh">
-    <no-ssr>
-      <l-map :zoom=13 :center="[47.413220, -1.219482]">
-        <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
-        <l-marker v-for="poi in pois" :lat-lng="poi" :key="poi.id"></l-marker>
-      </l-map>
-    </no-ssr>
+  <div>
+    <LocationChooser :use-current-position="true"
+                     :start-expanded="true"
+                     :title="'Where does your journey begin?'"
+                     :short-title="'Start'"
+                     v-on:accept="onAcceptStart"
+                     v-on:reset="">
+    </LocationChooser>
+    <LocationChooser :use-current-position="false"
+                     :title="'Where do you want to go?'"
+                     :short-title="'Destination'"
+                     v-on:accept="onAcceptDestination"
+                     v-on:reset="">
+    </LocationChooser>
+
+    <div class="columns">
+      <div v-if="startLocation && destinationLocation" class="column is-one-third">
+        <ul>
+          <li>Head left</li>
+          <li>Head right</li>
+          <li>Go straight</li>
+        </ul>
+      </div>
+      <div class="column">
+        <div id="map-wrap" style="height: 100vh">
+            <l-map ref="leafletMap" :zoom=13 :center="[48.134136, 11.588035]">
+              <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
+              <l-marker v-for="poi in pois" :lat-lng="poi" :key="poi.id"></l-marker>
+            </l-map>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
-<script>
-
-  export default {
-    mounted() {
-      this.$nextTick(() => {
-        console.log(this.$refs.leafletMap);
-      })
-    },
-    data() {
-      return {
-        pois: [
-          {
-            id:1,
-            lat: 47.413220,
-            lon: -1.219482
-          },
-          {
-            id:2,
-            lat: 46.413220,
-            lon: -1.219482
-          },
-          {
-            id:3,
-            lat: 45.413220,
-            lon: -1.219482
-          },
-          {
-            id:4,
-            lat: 44.413220,
-            lon: -1.219482
-          }
-        ]
-      }
-    },
-    components: {}
-  }
-</script>
-
 
 <style>
 
 </style>
+
+<script>
+
+  import LocationChooser from "../components/LocationChooser";
+
+  export default {
+    mounted() {
+    },
+    methods: {
+      onAcceptStart(loc) {
+        this.startLocation = loc;
+        this.refreshMap();
+        console.log("Accept start:", this.startLocation);
+      },
+      onAcceptDestination(loc) {
+        this.destinationLocation = loc;
+        this.refreshMap();
+        console.log("Accept dest:", this.destinationLocation);
+      },
+      refreshMap() {
+        this.pois = [];
+        if (this.startLocation && this.destinationLocation) {
+          this.$nextTick(() => {
+            if (process.browser) {
+              const leaflet = require('leaflet');
+              const leafletRoutingMachine = require('leaflet-routing-machine');
+              const map = this.$refs.leafletMap.mapObject;
+
+              L.Routing.control({
+                waypoints: [
+                  L.latLng(this.startLocation.lat, this.startLocation.lng),
+                  L.latLng(this.destinationLocation.lat, this.destinationLocation.lng)
+                ]
+              }).addTo(map);
+            }
+          })
+        } else if (this.startLocation) {
+          this.pois.push(this.startLocation);
+        } else if (this.destinationLocation) {
+          this.pois.push(this.destinationLocation);
+        }
+      }
+    },
+    data() {
+      return {
+        pois: [],
+        startLocation: null,
+        destinationLocation: null
+      }
+    },
+    components: {
+      LocationChooser
+    }
+  }
+</script>
