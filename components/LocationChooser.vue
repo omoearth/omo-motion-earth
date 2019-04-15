185430425eae8bbe96525fab86e74386b51be842
daniel <daniel.janz@dbi-analytics.de>
2019-04-16 00:10:53 +0200

    added a fallback to the user's current coordinates when the address lookup failed

--- components/LocationChooser.vue
+++ components/LocationChooser.vue
@@ -33,34 +33,36 @@
 
   export default {
     mounted() {
-      //this.$nextTick(() => {
-        if (!this.useCurrentPosition) {
-          return;
-        }
+      if (!this.useCurrentPosition) {
+        return;
+      }
 
-        this.currentPlaceholder = this.placeholder;
-        const self = this;
-        // Get the user's current location.
-        // If possible, set it as start coordinates, else just ignore the error and let the user choose.
-        navigator.geolocation.getCurrentPosition(function (pos) {
-          let url = "https://reverse.geocoder.api.here.com/6.2/reversegeocode.json?prox=" + pos.coords.latitude + "%2C" + pos.coords.longitude + "%2C250&mode=retrieveAddresses&maxresults=1&gen=9&app_id=cJoX1MQAOOQaqI3ezAR8&app_code=II15gTFlSok2GRWWHm0kIw";
+      this.currentPlaceholder = this.placeholder;
+      const self = this;
+      // Get the user's current location.
+      // If possible, set it as start coordinates, else just ignore the error and let the user choose.
+      navigator.geolocation.getCurrentPosition(function (pos) {
+        let url = "https://reverse.geocoder.api.here.com/6.2/reversegeocode.json?prox=" + pos.coords.latitude + "%2C" + pos.coords.longitude + "%2C250&mode=retrieveAddresses&maxresults=1&gen=9&app_id=cJoX1MQAOOQaqI3ezAR8&app_code=II15gTFlSok2GRWWHm0kIw";
 
-          self.$axios.$get(url).then(o => {
-            const address = o.Response.View[0].Result[0].Location.Address.Label;
-            const pos = o.Response.View[0].Result[0].Location.DisplayPosition;
-            self.location = {
-              label: address,
-              lat: pos.Latitude,
-              lng: pos.Longitude
-            };
-          }).catch(e => {
-            console.log("Reverse geocoding results:", "Error", e);
-          });
-        }, function(e) {
-          self.currentPlaceholder = "Where are you?";
+        self.$axios.$get(url).then(o => {
+          const address = o.Response.View[0].Result[0].Location.Address.Label;
+          const pos = o.Response.View[0].Result[0].Location.DisplayPosition;
+          self.location = {
+            label: address,
+            lat: pos.Latitude,
+            lng: pos.Longitude
+          };
+        }).catch(e => {
+          console.log("Reverse geocoding results:", "Error", e);
+          self.location = {
+            label: "Your current location",
+            lat: pos.coords.latitude,
+            lng: pos.coords.longitude
+          };
         });
-
-      //})
+      }, function(e) {
+        self.currentPlaceholder = "Where are you?";
+      });
     },
     props: [
       "title",
@@ -111,48 +113,6 @@
         isFetching:false,
         suggestions: [],
         recentLocations: [
-          {
-            label: "Adlzreiterstraße",
-            type: "Home",
-            lat: 48.125841,
-            lng: 11.555695
-          },
-          {
-            label: "Aventinstraße",
-            type: "Home",
-            lat: 48.133041,
-            lng: 11.579294
-          },
-          {
-            label: "Balanstraße",
-            type: "Home",
-            lat: 48.117359,
-            lng: 11.601246
-          },
-          {
-            label: "Sonnenstraße",
-            type: "Club",
-            lat: 48.136739,
-            lng: 11.565173
-          },
-          {
-            label: "Werner-Heisenberg-Allee",
-            type: "Stadium",
-            lat: 48.218647,
-            lng: 11.624767
-          },
-          {
-            label: "Von-Kahr-Straße",
-            type: "Gastro",
-            lat: 48.176360,
-            lng: 11.461615
-          },
-          {
-            label: "Englischer Garten",
-            type: "Park",
-            lat: 48.170138,
-            lng: 11.612823
-          }
         ],
         location: {
           lat: 48.170138,
