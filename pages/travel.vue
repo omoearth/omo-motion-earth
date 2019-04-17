21d287c3ed4a15f6d40dc0d269e0e9cb5372630d
daniel <daniel.janz@dbi-analytics.de>
2019-04-18 00:22:50 +0200

    * structured the 'travel' process like 'signup'

---
+++ pages/travel.vue
@@ -0,0 +1,35 @@
+<template>
+
+  <div>
+    <section class="columns">
+      <div class="column is-two-thirds">
+        <nuxt-child />
+      </div>
+      <div class="column sticky">
+        <aside class="menu sticky">
+          <p class="menu-label">
+            Steps
+          </p>
+          <ul class="menu-list">
+            <li>
+              <NuxtLink to="/travel/enter-route">enter start and destination</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/travel/open-vehicle">find and open the vehicle</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/travel/drive-vehicle">Drive to your destination</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/travel/park-vehicle">Park and lock the vehicle</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/travel/finish">Yeah! You made it! Still alive and healthy.</NuxtLink>
+            </li>
+          </ul>
+        </aside>
+      </div>
+    </section>
+  </div>
+
+</template>
