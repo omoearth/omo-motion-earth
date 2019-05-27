b099cd1ad82a8f9f33978de54853a123c9ef7849
philipp <philippbartels@outlook.com>
2019-05-27 10:41:58 +0200

    speedup for build process

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,15 +1,45 @@
+// nuxt.config.js
+var path = require('path')
+
+const fastSassLoader = {
+  loader: 'fast-sass-loader',
+  options: {
+    include: [
+      path.resolve(__dirname, 'assets/scss')
+    ]
+  }
+}
+
+function isVueRule(rule) {
+  return (rule.test.toString() === '/\\.vue$/')
+}
+
+function isSassRule(rule) {
+  return (['/\\.sass$/', '/\\.scss$/'].indexOf(rule.test.toString()) !== -1)
+}
 export default {
   mode: 'universal',
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: "omo earth",
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: "the power of movement" },
-      { name: "theme-color", content: "#ffffff" }
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: "the power of movement"
+      },
+      {
+        name: "theme-color",
+        content: "#ffffff"
+      }
     ],
     // link: [
     //   { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
@@ -21,47 +51,54 @@ export default {
     // ]
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: {
+    color: '#fff'
+  },
 
   router: {
     middleware: 'default-routes'
   },
 
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    { src: '~plugins/nuxt-vue-select', ssr: false },
-    { src: '~plugins/nuxt-swiper-plugin', ssr: false },
+   ** Plugins to load before mounting the App
+   */
+  plugins: [{
+      src: '~plugins/nuxt-vue-select',
+      ssr: false
+    },
+    {
+      src: '~plugins/nuxt-swiper-plugin',
+      ssr: false
+    },
   ],
 
   /*
-  ** Nuxt.js modules
-  */
-  modules: [ 
-    'nuxt-buefy', 
-    'nuxt-fontawesome', 
-    'nuxt-leaflet', 
-    '@nuxtjs/apollo', 
+   ** Nuxt.js modules
+   */
+  modules: [
+    'nuxt-buefy',
+    'nuxt-fontawesome',
+    'nuxt-leaflet',
+    '@nuxtjs/apollo',
     '@nuxtjs/axios',
     '@nuxtjs/pwa',
     '@nuxtjs/style-resources'
   ],
 
-  styleResources: {
-    scss: [
-      './assets/theme.scss',
-      ]
-  },
+  // styleResources: {
+  //   scss: [
+  //     './assets/theme.scss',
+  //   ]
+  // },
 
   css: [
     // (...)
     './assets/theme.scss',
     'swiper/dist/css/swiper.css'
   ],
-  
+
   apollo: {
     clientConfigs: {
       default: {
@@ -71,23 +108,36 @@ export default {
     }
   },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
+     ** You can extend webpack config here
+     */
     extend(config, ctx) {
+      config.module.rules.forEach((rule) => {
+        if (isVueRule(rule)) {
+          rule.options.loaders.sass.push(fastSassLoader)
+          rule.options.loaders.scss.push(fastSassLoader)
+        }
+
+        if (isSassRule(rule)) {
+          rule.use.push(fastSassLoader)
+        }
+      })
     }
   },
-  
-  fontawesome: {
-    imports: [
-      {
-        set: '@fortawesome/free-solid-svg-icons',
-        icons: ['fas']
-      },
-    ],
-  },
-}
 
+  fontawesome: {
+    imports: [{
+      set: '@fortawesome/free-solid-svg-icons',
+      icons: ['fas']
+    }, ],
+  },
+
+  generate: {
+    workers: 4,
+    workerConcurrency: 500,
+    concurrency: 500,
+  }
+}
