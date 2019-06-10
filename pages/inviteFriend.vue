33acc9e5fd870a7e4f722c422b2408b0b909412a
Samuel Andert <samuelandert@me.com>
2019-06-10 19:33:01 +0200

    added invite friend page

--- pages/inviteFriend.vue
+++ pages/inviteFriend.vue
@@ -3,39 +3,44 @@
     <div class="hero">
       <div class="hero-body">
         <div class="container has-text-centered">
-          <h1 class="title">INSPIRE FRIEND</h1>
-          <h1 class="subtitle">to win the greencity race</h1>
+          <h1 class="title">
+            INSPIRE FRIEND
+          </h1>
+          <h1 class="subtitle">
+            to win the greencity race
+          </h1>
         </div>
       </div>
     </div>
-   
- <ApolloMutation :mutation="require('../apollo/mutation/inviteFriend.gql')"
+
+    <ApolloMutation
+      :mutation="require('../apollo/mutation/inviteFriend.gql')"
       :variables="{name: name, email: email, city: city}"
     >
-    <div class="field">
-      <div class="control">
-        <input class="input is-large" type="text" placeholder="name" v-model="name">
+      <div class="field">
+        <div class="control">
+          <input v-model="name" class="input is-large" type="text" placeholder="name">
+        </div>
       </div>
-    </div>
-     <div class="field">
-      <div class="control">
-        <input class="input is-large" type="text" placeholder="city" v-model="city">
+      <div class="field">
+        <div class="control">
+          <input v-model="city" class="input is-large" type="text" placeholder="city">
+        </div>
       </div>
-    </div>
-    <div class="field">
-      <div class="control">
-        <input class="input is-large" type="email" placeholder="email" v-model="email">
+      <div class="field">
+        <div class="control">
+          <input v-model="email" class="input is-large" type="email" placeholder="email">
+        </div>
       </div>
-    </div>
-       <template v-slot="{ mutate, loading, error }">
+      <template v-slot="{ mutate, loading, error }">
         <button class="button is-primary is-medium" :disabled="loading" @click="mutate()">
-         SEND INVITE
+          SEND INVITE
         </button>
         <p v-if="error">
           An error occurred: {{ error }}
         </p>
       </template>
-     </ApolloMutation>
+    </ApolloMutation>
   </div>
 </template>
 
@@ -44,9 +49,9 @@
 export default {
   data() {
     return {
-      name: "",
-      city: "",
-      email: ""
+      name: '',
+      city: '',
+      email: ''
     }
   }
 }
