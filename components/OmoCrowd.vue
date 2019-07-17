ea18fb75f0e0f77e8045aed30fd21d9e830efaa9
Samuel Andert <samuelandert@me.com>
2019-07-17 16:10:21 +0200

    minor color change

--- components/OmoCrowd.vue
+++ components/OmoCrowd.vue
@@ -1,10 +1,8 @@
 <template>
   <div
-    class="has-text-centered title is-size-7-mobile is-size-6-tablet is-size-6-desktop has-text-light has-background-dark"
+    class="has-text-centered title is-size-7-mobile is-size-6-tablet is-size-6-desktop has-text-light has-background-primary"
     style="padding: 0.6rem"
-  >
-    crowdfunding pre-sale kickstarts when we reach 100.000 greenfluencer
-  </div>
+  >crowdfunding pre-sale kickstarts when we reach 100.000 greenfluencer</div>
 </template>
 
 <script lang="ts">
