d965964963fdb80b3390a0955132086cbd3fb43c
Samuel Andert <samuelandert@me.com>
2019-07-19 12:13:07 +0200

    minor text

--- components/OmoMission.vue
+++ components/OmoMission.vue
@@ -6,9 +6,7 @@
           <div
             class="title is-size-1 is-size-1-tablet is-size-4-mobile"
             style="padding-bottom: 1rem"
-          >
-            THE MISSION
-          </div>
+          >THE MISSION</div>
           <div
             class="subtitle has-text-weight-medium is-size-2 is-size-4-tablet is-size-6-mobile has-text-dark"
           >
@@ -42,7 +40,7 @@ import { Component, Vue } from "nuxt-property-decorator";
         },
         {
           title: "2. CROWDFUND",
-          subtitle: "and unlock new mobility providers with each 100.000€",
+          subtitle: "and vote new mobility providers with each 100.000€",
           icon: "vote"
         },
         {
