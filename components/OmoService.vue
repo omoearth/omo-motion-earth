b3a478d1666e0066a3739d1b6eac5535a732b2dc
Samuel Andert <samuelandert@me.com>
2019-07-23 17:30:18 +0200

    changed vision to goal

--- components/OmoService.vue
+++ components/OmoService.vue
@@ -5,7 +5,7 @@
         <div
           class="title is-size-1 is-size-1-tablet is-size-4-mobile"
           style="padding-bottom: 1rem"
-        >OUR VISION</div>
+        >OUR GOAL</div>
         <div
           class="subtitle has-text-weight-medium is-size-2 is-size-4-tablet is-size-6-mobile has-text-dark"
         >enjoy the freedom to access, drive and leave any green sharing vehicle from any place in the world</div>
